6b7f0b5f3e97b4bbdbbe01265a441989060b9a23
Samyak1202 <99115614+Samyak1202@users.noreply.github.com>
2022-07-14 21:16:10 +0530

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,2 @@
-export const API_KEY = "AIzaSyBVhuhQK65vlHzQh5NaGsTB0cIk63BTPJI";
+export const API_KEY = "Enter your own api key";
 export default API_KEY;
-// AIzaSyBVhuhQK65vlHzQh5NaGsTB0cIk63BTPJI
-// -----custom---
-// d0f280c47216fbe01
